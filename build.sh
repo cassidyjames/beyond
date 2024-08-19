e6855a3bf19987c561c453c43f7fc9826d368c2d
Cassidy James Blaede <c@ssidyjam.es>
2024-08-19 10:12:40 -0600

    Packages: I forgot how to bash

--- build.sh
+++ build.sh
@@ -9,19 +9,11 @@ RELEASE="$(rpm -E %fedora)"
 
 ## Remove Fedora packages that will be replaced by Flatpaks or other packages
 rpm-ostree override remove \
-
-  # Replaced by GNOME Web/can install Firefox from Flatpak
   firefox firefox-langpacks \
-
-  # We don't need a system extension for a logo.. or other random things
   gnome-shell-extension-apps-menu gnome-shell-extension-background-logo \
   gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu \
   gnome-shell-extension-window-list \
-
-  # Replaced by Ptyxis
   gnome-terminal gnome-terminal-nautilus \
-
-  # Create ugly .desktop files for minimal value
   htop nvtop
 
 ## Packages that don't work as Flatpaks from Fedora repos
